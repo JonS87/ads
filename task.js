0c86d9312a112536165c4d22866ec96e5d19f902
Evgeniy Chausov <evgeniy@chausov.ru>
2024-03-04 11:52:56 +0300

    Done HW

--- task.js
+++ task.js
@@ -1,16 +1,24 @@
 const rotatorCase = document.querySelectorAll('.rotator__case');
-const i = 0;
+let i = 0;
+let speed = (+document.getElementsByClassName('rotator__case_active')[0].getAttribute('data-speed'));
 
-setInterval(() => {
+let timer = setTimeout(function updateHtml() {
   if (i === rotatorCase.length) {
     i = 0;
   }
-  if (i > 0 & rotatorCase[i - 1].contains('rotator__case_active')) {
-    rotatorCase[i - 1].remove('rotator__case_active');
-  } else if (i = 0 & rotatorCase[rotatorCase.length - 1].contains('rotator__case_active')) {
-    rotatorCase[rotatorCase.length - 1].remove('rotator__case_active')
+  if (i > 0 && rotatorCase[i - 1].className.includes('rotator__case_active')) {
+      rotatorCase[i - 1].classList.remove('rotator__case_active');
+  } else if (i === 0 && rotatorCase[rotatorCase.length - 1].className.includes('rotator__case_active')) {
+    rotatorCase[rotatorCase.length - 1].classList.remove('rotator__case_active')
   }
+  
+  rotatorCase[i].classList.add('rotator__case_active');
+  const span = document.getElementsByClassName('rotator__case_active')[0];
+  speed = (+span.getAttribute('data-speed'));
+  const color = span.getAttribute('data-color');
+  span.style.color = color
+  console.log(speed, Date());
+  timer = setTimeout(updateHtml, speed);
 
-  rotatorCase[i].add('rotator__case_active');
-  i++;
+  i = i + 1;
 }, 1000)
